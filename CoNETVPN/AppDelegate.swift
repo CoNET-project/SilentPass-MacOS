d11a6498a2f2f764cc8f3c4d0c903daa6913ad54
yangxu <yang.xu@fx168group.com>
2024-11-23 23:52:32 +0800

    Initial Commit

---
+++ CoNETVPN/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  CoNETVPN
+//
+//  Created by 杨旭的MacBook Pro on 2024/11/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ CoNETVPN/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CoNETVPN
+//
+//  Created by 杨旭的MacBook Pro on 2024/11/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
